360c25e59bc553c41befb5a8e350fb59545cc42a
Caleb Maclennan <caleb@alerque.com>
2026-06-30 18:10:54 +0300

    fixup! feat(tooling): Add Nix flake

--- flake.nix
+++ flake.nix
@@ -66,16 +66,32 @@
             rustc = pkgs.rust-bin.stable.latest.minimal;
           };
 
+          sourceWithGit =
+            # If using a locked (clean) input, clone from remote with history
+            if self ? rev && self.rev != null then
+              builtins.fetchGit {
+                url = self.sourceInfo.url;
+                rev = self.rev;
+                allRefs = true;
+                leaveDotGit = true;
+              }
+            # If using a dirty local input, clone from local working copy with history
+            else if builtins.pathExists (toString ./. + "/.git") then
+              builtins.fetchGit {
+                url = toString ./.;
+                allRefs = true;
+                leaveDotGit = true;
+              }
+            # Fallback for `git archive` generated tarballs without git history
+            else
+              ./.;
+
           rustPackage =
             features:
             rustPlatform.buildRustPackage {
               name = resolveManifestValue "name";
               version = resolveManifestValue "version";
-              src = lib.cleanSourceWith {
-                src = ./.;
-                filter = path: type:
-                  baseNameOf path == ".git" || lib.cleanSourceFilter path type;
-              };
+              src = sourceWithGit;
               cargoLock.lockFile = ./Cargo.lock;
               buildFeatures = features;
               buildInputs = runtimeDeps;
