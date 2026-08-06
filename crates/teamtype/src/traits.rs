db4afc558da59c22870a8633aa4a53a230a20e3a
Caleb Maclennan <caleb@alerque.com>
2026-08-06 15:10:19 +0300

    feat(crates): Add Debug trait to UI implementations

--- crates/teamtype/src/traits.rs
+++ crates/teamtype/src/traits.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::any::type_name;
+
 /// Defines the ways Teamtype expects to be able to interact with an end user. This includes
 /// printing messages through *to* the user and receiving confirmations *from* them. When using the
 /// CLI interface, these interactions are provided as console functions that interact with the
@@ -9,4 +11,10 @@
 ///
 /// Editors or editor plugins directly linking to Teamtype will need to wire these up to whatever
 /// UI mechanism is available in their context.
-pub trait Interactions: Send + Sync {}
+pub trait Interactions: Send + Sync {
+    /// Enable a blanket [`Debug`] implementation for [`UserInterface`] by returning the name of
+    /// the UI struct in use.
+    fn type_name(&self) -> &'static str {
+        type_name::<Self>()
+    }
+}
